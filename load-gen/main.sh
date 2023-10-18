777b49bde26e03ccbedf370b792670fbce7a5001
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-18 16:36:05 -0400

    updated parameter-store jenkinsfile

---
+++ load-gen/main.sh
@@ -0,0 +1,9 @@
+requests=$1
+url=$2
+
+curl -s "https://random-word-api.herokuapp.com/word?number=$requests" | sed -e "s/\[//" -e 's/\]//' | sed -e 's/,/ /g' -e 's/"//g' >/tmp/words
+
+for word in `cat /tmp/words`; do
+  curl -X POST $url/api/transaction --header "Content-Type: application/json" -d "{\"amount\":\"$RANDOM\",\"desc\":\"$word\"}" &>/tmp/out
+  curl -X DELETE https://prod.yamunadevops.online/api/transaction &>>/tmp/out
+done
